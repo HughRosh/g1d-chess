14aff7f2200895202ba4ef2d7cb0caba1d6874a6
HughRosh <Hugh@americanrobots.com>
2026-07-08 16:10:45 -0400

    Add dry run robot controller

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from board_detector import detect_board_state
 from chess_engine import choose_move
 from board_geometry import move_to_pick_place
 from arm_planner import make_pick_place_motion
+from robot_controller import execute_motion
 
 def main():
     board = detect_board_state()
@@ -15,10 +16,8 @@ def main():
     print("G1-D Chess")
     print(f"Chosen move: {move.uci()}")
     print(f"Board FEN after move: {board.fen()}")
-    print("Motion plan:")
 
-    for step in motion:
-        print(step)
+    execute_motion(motion, dry_run=True)
 
 if __name__ == "__main__":
     main()
